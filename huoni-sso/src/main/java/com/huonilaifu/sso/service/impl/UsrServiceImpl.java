48902018dc5f9630049fa455feeb719562749f69
lirb <lirb0121@163.com>
2019-04-17 14:07:35 +0800

    'sdf'

--- huoni-sso/src/main/java/com/huonilaifu/sso/service/impl/UsrServiceImpl.java
+++ huoni-sso/src/main/java/com/huonilaifu/sso/service/impl/UsrServiceImpl.java
@@ -5,10 +5,13 @@ import com.huonilaifu.sso.model.User;
 import com.huonilaifu.sso.model.UserExample;
 import com.huonilaifu.sso.service.UsrService;
 import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author: lirb
@@ -20,6 +23,8 @@ public class UsrServiceImpl implements UsrService {
 
     @Resource
     private UserMapper userMapper;
+    @Resource
+    private RedisTemplate redisTemplate;
 
     public User selectByUsernameAndPassword(String username, String password) {
         UserExample userExample = new UserExample();
@@ -28,6 +33,13 @@ public class UsrServiceImpl implements UsrService {
         criteria.andPasswordEqualTo(password);
         List<User> users = userMapper.selectByExample(userExample);
         if(CollectionUtils.isNotEmpty(users)&&users.size()==1){
+            User user = users.get(0);
+            String token = "asdf1234";
+            Map<String, String> dataMap = new HashMap<String, String>();
+            dataMap.put("username",user.getUsername());
+            dataMap.put("password", user.getPassword());
+            redisTemplate.opsForValue().set(token+"token",token);
+            redisTemplate.opsForHash().putAll(token,dataMap);
             return users.get(0);
         }
         return null;
--- huoni-web/src/main/java/com/huonilaifu/web/sso/UsrController.java
+++ huoni-web/src/main/java/com/huonilaifu/web/sso/UsrController.java
@@ -9,6 +9,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author: lirb
@@ -23,23 +31,34 @@ public class UsrController {
     private UsrService userService;
 
     @RequestMapping("/index")
-    public ModelAndView index(){
+    public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         return modelAndView;
     }
 
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public ModelAndView login(@RequestParam(name = "username")String username,
-                              @RequestParam(name = "password")String password){
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public void login(HttpServletRequest req, HttpServletResponse response,
+                              @RequestParam(name = "username") String username,
+                              @RequestParam(name = "password") String password) throws ServletException, IOException {
         ModelAndView modelAndView = new ModelAndView();
 
         User user = userService.selectByUsernameAndPassword(username, password);
-        if(user != null){
-            modelAndView.setViewName("success");
-            return modelAndView;
+        PrintWriter out = response.getWriter();
+        if (user != null) {
+            HttpSession session = req.getSession();
+            session.setMaxInactiveInterval(60*30);
+            session.setAttribute("user",user.getUsername());
+            Cookie cookie = new Cookie("token","asdf1234");
+            response.addCookie(cookie);
+            req.getRequestDispatcher("/WEB-INF/jsp/upload.jsp").forward(req,response);
+//            modelAndView.setViewName("upload");
+//            return modelAndView;
+        }else {
+            out.write("<br/>欢迎你，登陆成功，3秒跳转首页");
+            response.setHeader("refresh", "1;url=/WEB-INF/jsp/login.jsp");
         }
-        modelAndView.setViewName("error");
-        return modelAndView;
+//        modelAndView.setViewName("login");
+//        return modelAndView;
     }
 }
